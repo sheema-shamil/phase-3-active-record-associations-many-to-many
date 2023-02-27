2bff294420f761ff284b9ccac8d341444c36ff91
sheema <sheemashamilfarook@gmail.com>
2023-02-27 10:12:47 +0300

    done

--- db/schema.rb
+++ db/schema.rb
@@ -14,19 +14,17 @@ ActiveRecord::Schema.define(version: 2021_07_19_113221) do
 
   create_table "games", force: :cascade do |t|
     t.string "title"
-    t.string "genre"
-    t.string "platform"
-    t.integer "price"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.integer "game_id"
+    t.datetime "create_at", precision: 6, null: false
+    t.datetime "update_at", precision: 6, null: false
+    t.integer "user_id"
+
   end
 
-  create_table "reviews", force: :cascade do |t|
-    t.integer "score"
-    t.string "comment"
-    t.integer "game_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table "users", force: :cascade do |t|
+   t.string "name"
+   t.datetime "create_at", precision: 6, null: false
+   t.datetime "update_at", precision: 6, null: false
   end
 
 end
